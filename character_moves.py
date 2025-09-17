bcc8f6d1f20c6a0220f50c011cc0929f05fee099
seunggyunJung <csg1221sg@gmail.com>
2025-09-17 17:03:55 +0900

    삼각운동 함수 완성

--- character_moves.py
+++ character_moves.py
@@ -11,8 +11,9 @@ character = load_image('character.png')
 c_x =400
 c_y =90
 
-moves=0
+moves=1
 speed =10
+line =0
 
 def move_ractangle(x,y):
     if y==90 and x < 780:
@@ -24,6 +25,26 @@ def move_ractangle(x,y):
     elif x==20 and y >90:
         return x, y- speed
 
+def move_triangle(x,y,line):
+    if line ==0:
+        y += 1.73 * speed
+        x += speed
+        if y>550:
+            line =1
+        return x,y,line
+    elif line ==1:
+        x-= speed
+        if x<105:
+            line =2
+        return x,y,line
+    elif line ==2:
+        y -= 1.73 * speed
+        x += speed
+        if y<=90:
+            line =-1
+        return x,y,line
+    elif line ==-1:
+        return 400,90,0
 
 while True:
     clear_canvas_now()
@@ -33,11 +54,12 @@ while True:
     if moves == 0:
         c_x,c_y = move_ractangle(c_x,c_y)
 
-    # elif moves == 1:
-    #     move_triangle(c_x,c_y)
+    elif moves == 1:
+        c_x,c_y,line = move_triangle(c_x,c_y,line)
     #
     # elif moves == 2:
     #     move_circle(c_x,c_y)
+
 
     delay(0.01)
 
