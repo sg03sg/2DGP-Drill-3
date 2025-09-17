9ddf9354c658bb2676db051d5ae442d16a4d2a4f
seunggyunJung <csg1221sg@gmail.com>
2025-09-17 14:59:02 +0900

    무한루프 뼈대 구성

--- character_moves.py
+++ character_moves.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import math
 
+from pico2d import clear_canvas_now
 
 open_canvas()
 
@@ -10,7 +11,21 @@ character = load_image('character.png')
 c_x =400
 c_y =90
 
+moves=0
 
-# fill here
+while True:
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(c_x,c_y)
 
-close_canvas()
+    if moves == 0:
+        move_ractangle(c_x,c_y)
+
+    elif moves == 1:
+        move_triangle(c_x,c_y)
+
+    elif moves == 2:
+        move_circle(c_x,c_y)
+
+    delay(0.01)
+
