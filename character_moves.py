f1cf6b3f7af1208f0de58139898ac8f0cc679bb4
seunggyunJung <csg1221sg@gmail.com>
2025-09-17 16:41:47 +0900

    사각운동 함수 완성

--- character_moves.py
+++ character_moves.py
@@ -12,6 +12,18 @@ c_x =400
 c_y =90
 
 moves=0
+speed =10
+
+def move_ractangle(x,y):
+    if y==90 and x < 780:
+         return x + speed, y
+    elif x==780 and y < 550:
+        return x, y+ speed
+    elif y==550 and x >20:
+        return x- speed, y
+    elif x==20 and y >90:
+        return x, y- speed
+
 
 while True:
     clear_canvas_now()
@@ -19,13 +31,13 @@ while True:
     character.draw_now(c_x,c_y)
 
     if moves == 0:
-        move_ractangle(c_x,c_y)
+        c_x,c_y = move_ractangle(c_x,c_y)
 
-    elif moves == 1:
-        move_triangle(c_x,c_y)
-
-    elif moves == 2:
-        move_circle(c_x,c_y)
+    # elif moves == 1:
+    #     move_triangle(c_x,c_y)
+    #
+    # elif moves == 2:
+    #     move_circle(c_x,c_y)
 
     delay(0.01)
 
