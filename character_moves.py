2052d9df5fc813cb9c393da406cad76662a24f7c
seunggyunJung <csg1221sg@gmail.com>
2025-09-17 14:54:35 +0900

    이미지 파일 불러오기 및 캐릭터 시작 좌표 초기화

---
+++ character_moves.py
@@ -0,0 +1,16 @@
+from pico2d import *
+import math
+
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+c_x =400
+c_y =90
+
+
+# fill here
+
+close_canvas()
